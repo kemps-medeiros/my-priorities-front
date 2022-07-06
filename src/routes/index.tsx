02c62ff92a1ff109588eb029765cb1699162a531
Kemps Medeiros <kempsmm@gmail.com>
2022-07-06 19:05:41 -0300

    deleted some unecessaries lines

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,7 @@ import AuthContext from "../contexts/auth";
 
 import SignRoutes from "./SignRoutes";
 import OtherRoutes from "./OtherRoutes";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../pages/Login";
-import Home from "../pages/Home";
+
 
 const AppRoutes: React.FC = () => {
     const { signed } = useContext(AuthContext);
