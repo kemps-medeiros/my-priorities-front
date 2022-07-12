0f6bf80b39553f655022fb11ab732930bfb2b59d
Kemps Medeiros <kempsmm@gmail.com>
2022-07-12 11:46:56 -0300

    refactor: deleted unnecessary lines

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,10 @@
-import React, { useContext } from "react";
-import AuthContext from "../../contexts/auth";
+import React from "react";
 
 import Navbar from "../../components/navbar/navbar";
 import "./style.css";
 import LoginCard from "../../components/login-card/login-card";
 
 const Login: React.FC = () => {
-    const context = useContext(AuthContext);
-
-    async function handleLogin() {
-        await context.Login();
-
-    }
 
     return (
         <div>
@@ -21,10 +14,7 @@ const Login: React.FC = () => {
                     <h3 className="title">Hi, Welcome to MyPrioties App!!</h3>
                     <h4 className="sub-title">This application was made for you, who like me, likes to organize your tasks in order of priority</h4>
                 </div>
-
                 <LoginCard />
-                <h1>Login</h1>
-                <button onClick={handleLogin}>Login</button>
             </div>
         </div>
     )
