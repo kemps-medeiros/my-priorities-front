1e7a9321aa94a2976e25096c973cb88d0670fe1a
Kemps Medeiros <kempsmm@gmail.com>
2022-07-11 21:56:33 -0300

    feat: created login-card

---
+++ src/components/login-card/login-card.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style.css";
+
+
+const LoginCard: React.FC = () => {
+
+    return (
+        <div>
+            <div className="row mt-50">
+                <div className="col s12 l4 offset-l4">
+                    <div className="card">
+                        <div className="card-action pink darken-4 white-text center">
+                            <h4>MyPriorities Login</h4>
+                        </div>
+                        <div className="card-content">
+                            <div className="form-field">
+                                <label htmlFor="email" >Email</label>
+                                <input type="email" id="email" />
+                            </div>
+                            <div className="form-field">
+                                <label htmlFor="password" >Password</label>
+                                <input type="password" id="password" />
+                            </div>
+                            <div className="form-field center">
+                                <button className="btn-large green accent-4">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="center">
+                        <a href="#" className="btn">
+                            Sign Up!
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div >
+    )
+}
+
+export default LoginCard;
