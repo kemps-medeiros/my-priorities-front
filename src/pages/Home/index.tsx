8bf24bf38d74ee2962299e854e9c6e2b017ef16e
Kemps Medeiros <kempsmm@gmail.com>
2022-07-12 22:08:31 -0300

    feat: edited page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import Navbar from "../../components/navbar/navbar";
+import NewTaskForm from "../../components/new-task-form/new-task-form";
 import AuthContext from "../../contexts/auth";
+import "./style.css";
 
 const Home: React.FC = () => {
     const context = useContext(AuthContext);
@@ -12,8 +14,19 @@ const Home: React.FC = () => {
     return (
         <div>
             <Navbar />
-            <h1>Home</h1>
-            <button onClick={handleLogout}>LogOut</button>
+            <div className="container">
+                <div className="row mt-20 right-align">
+                    <button
+                        className="btn-large red accent-4 logout"
+                        onClick={handleLogout}>
+                        LogOut
+                    </button>
+                    <div className="row">
+                        <NewTaskForm />
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
