8048ab4324f78b24507892bebdd1140a8b090f34
Kemps Medeiros <kempsmm@gmail.com>
2022-07-21 18:26:58 -0300

    fix: transfor menu respomsive

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 
 
+
 const Navbar: React.FC = () => {
 
     return (
@@ -9,28 +10,24 @@ const Navbar: React.FC = () => {
             <nav className="pink darken-4">
                 <div className="container">
                     <div className="nav-wrapper">
-                        <a href="#" className="brand-logo">MyPriorities</a>
-                        {/* Quando for implementar o meu responsivo, utilizar: */}
-                        {/* <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+                        <a href="#!" className="brand-logo">MyPriorities</a>
+                        <a href="#" data-target="mobile-demo" className="sidenav-trigger">
                             <i className="material-icons">menu</i>
                         </a>
                         <ul className="right hide-on-med-and-down">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">Javascript</a></li>
-                            <li><a href="mobile.html">Mobile</a></li>
-                        </ul> 
-                        verificar DOC do Materialize
-                        */}
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><a target="_blank" rel="noreferrer" href="https://github.com/kemps-medeiros">Github</a></li>
-                            <li><a target="_blank" rel="noreferrer" href="https://kemps-medeiros.github.io/kemps-portfolio/">Portfolio</a></li>
-                            <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/kempsmedeiros/">Linkedin</a></li>
+                            <li><a target="_blank" rel="noreferrer" href="https://github.com/kemps-medeiros">My-Github</a></li>
+                            <li><a target="_blank" rel="noreferrer" href="https://kemps-medeiros.github.io/kemps-portfolio/">My-Portfolio</a></li>
+                            <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/kempsmedeiros/">My-Linkedin</a></li>
                         </ul>
                     </div>
                 </div>
-            </nav>
-        </div>
+            </nav >
+            <ul className="sidenav" id="mobile-demo">
+                <li><a target="_blank" rel="noreferrer" href="https://github.com/kemps-medeiros">My-Github</a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://kemps-medeiros.github.io/kemps-portfolio/">My-Portfolio</a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/kempsmedeiros/">My-Linkedin</a></li>
+            </ul>
+        </div >
     )
 }
 
