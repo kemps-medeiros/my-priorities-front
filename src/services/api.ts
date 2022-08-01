ed03105f88b3744446b39440e06a1b660675f24e
Kemps Medeiros <kempsmm@gmail.com>
2022-08-01 20:08:15 -0300

    fix: fix env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,8 @@ const hostname = process.env.REACT_APP_HOSTNAME;
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/myprioritiesapp",
-  baseURL: hostname,
+  baseURL:
+    "http://my-priorities.us-east-1.elasticbeanstalk.com/myprioritiesapp",
 });
 
 export default api;
