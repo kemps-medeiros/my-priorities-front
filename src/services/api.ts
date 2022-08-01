a44bcac1676fda2bb229386064fc7da143b9e7dd
Kemps Medeiros <kempsmm@gmail.com>
2022-08-01 16:25:28 -0300

    feat: take note of backend url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// http://my-priorities.us-east-1.elasticbeanstalk.com/
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/myprioritiesapp',
+  baseURL: "http://localhost:5000/myprioritiesapp",
 });
 
 export default api;
